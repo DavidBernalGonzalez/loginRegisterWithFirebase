cc5b707cbf80e72c4658565af91f4f7000f1a335
David <eldeividlh@gmail.com>
2021-01-06 22:22:19 +0100

    Refactor auth login Apps and resolve bugs validator field password

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
 import { Router } from '@angular/router';
-import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(public afAuth: AngularFireAuth, public router: Router, private snackBar: MatSnackBar) {}
+  constructor(
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'bottom';
@@ -18,7 +26,7 @@ export class AuthService {
       duration: 5000,
       horizontalPosition: this.horizontalPosition,
       verticalPosition: this.verticalPosition,
-      panelClass: ['mat-toolbar', 'mat-warn']
+      panelClass: ['mat-toolbar', 'mat-warn'],
     });
 
     // snack.afterDismissed().subscribe(() => {
@@ -30,7 +38,7 @@ export class AuthService {
     });
   }
 
-  async onLoginWithEmail(email: string, password: string): Promise<any> {
+  onLoginWithEmail(email: string, password: string): void {
     try {
       console.log('Email&password login...');
       this.afAuth
@@ -41,29 +49,24 @@ export class AuthService {
         })
         .catch((err) => {
           console.log(err);
-          this.openSnackBar('Error in login! Try other time or go to ', 'Register', '/register');
+          this.openSnackBar(
+            'Error in login! Try other time or go to ',
+            'Register',
+            '/register'
+          );
         });
     } catch (err) {
       return err;
     }
   }
 
-    // Sign in with Facebook
-    // Go to https://developers.facebook.com/docs/facebook-login/
-    // Create App and asociate your account paste ID App + Secret App in Firebase and paste URI OAuth in Facebook App
-  async onLoginWithFacebook(): Promise<any> {
+  // Sign in with Facebook
+  // Go to https://developers.facebook.com/docs/facebook-login/
+  // Create App and asociate your account paste ID App + Secret App in Firebase and paste URI OAuth in Facebook App
+  onLoginWithFacebook(): void {
     try {
-      console.log('Facebook login...');
-      this.afAuth
-        .signInWithPopup(new firebase.auth.FacebookAuthProvider())
-        .then((response) => {
-          console.log(response);
-          this.router.navigate(['/welcome']);
-        })
-        .catch((err) => {
-          console.log(err);
-          this.openSnackBar('Error in login! Try other time or go to ', 'Register', '/register');
-        });
+      console.log('Try to loggin with Twitter...');
+      this.loggin(new firebase.auth.FacebookAuthProvider());
     } catch (err) {
       return err;
     }
@@ -73,19 +76,10 @@ export class AuthService {
   // Sign in with Twitter
   // Go to https://developer.twitter.com/en/apps
   // Create App and asociate your account paste ID App + Secret App in Firebase and paste URI OAuth in Facebook App
-  async onLoginWithTwitter(): Promise<any> {
+  onLoginWithTwitter(): void {
     try {
-      console.log('Twitter login...');
-      this.afAuth
-        .signInWithPopup(new firebase.auth.TwitterAuthProvider())
-        .then((response) => {
-          console.log(response);
-          this.router.navigate(['/welcome']);
-        })
-        .catch((err) => {
-          console.log(err);
-          this.openSnackBar('Error in login! Try other time or go to ', 'Register', '/register');
-        });
+      console.log('Try to loggin with Twitter...');
+      this.loggin(new firebase.auth.TwitterAuthProvider());
     } catch (err) {
       return err;
     }
@@ -95,19 +89,10 @@ export class AuthService {
   // Go to https://docs.github.com/en/free-pro-team@latest/developers/apps/building-oauth-apps
   // In your acount user go to Settings > Developers Settings > OAuth Apps > Register a new application
   // Create App and paste ID App + Generate a new client secret in Firebase and paste URI OAuth in GitHub App
-  async onLoginWithGitHub(): Promise<any> {
+  onLoginWithGitHub(): void {
     try {
-      console.log('Twitter login...');
-      this.afAuth
-        .signInWithPopup(new firebase.auth.GithubAuthProvider())
-        .then((response) => {
-          console.log(response);
-          this.router.navigate(['/welcome']);
-        })
-        .catch((err) => {
-          console.log(err);
-          this.openSnackBar('Error in login! Try other time or go to ', 'Register', '/register');
-        });
+      console.log('Try to loggin with GitHub...');
+      this.loggin(new firebase.auth.GithubAuthProvider());
     } catch (err) {
       return err;
     }
@@ -118,18 +103,31 @@ export class AuthService {
   // And go to Crear credenciales > Id de cliente de OAuth
   // Credenciales > Clave de API o IDs de cliente de OAuth 2.0
   // And paste ID App in Firebase
-  async onLoginWithGoogle(): Promise<any> {
+  onLoginWithGoogle(): void {
     try {
-      console.log('Twitter login...');
+      console.log('Try to loggin with Google...');
+      this.loggin(new firebase.auth.GoogleAuthProvider());
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async loggin(app: any): Promise<any> {
+    try {
       this.afAuth
-        .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+        .signInWithPopup(app)
         .then((response) => {
           console.log(response);
+          console.log('Login sucess');
           this.router.navigate(['/welcome']);
         })
         .catch((err) => {
           console.log(err);
-          this.openSnackBar('Error in login! Try other time or go to ', 'Register', '/register');
+          this.openSnackBar(
+            'Error in login! Try other time or go to ',
+            'Register',
+            '/register'
+          );
         });
     } catch (err) {
       return err;
